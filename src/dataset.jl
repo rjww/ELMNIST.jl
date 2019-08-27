04bb571d9f77e6fd51c0e4c7697994976e59cef5
Robert Woods <git@robertwoods.me>
2019-08-27 16:33:58 +0930

    Refactor data-fetching methods

--- src/dataset.jl
+++ src/dataset.jl
@@ -1,46 +1,72 @@
 const download = MLDatasets.MNIST.download
 
-# TODO Mimic MLDatasets API more closely.
 function traindata(; dir = nothing)
     X, T = MLDatasets.MNIST.traindata(dir = dir)
-    rows, cols, n = size(X)
-    (reshape(X, (rows * cols, n)), T)
+    reshape_data(X, T)
+end
+
+function traindata(n_samples::Int; dir = nothing)
+    X, T = MLDatasets.MNIST.traindata(dir = dir)
+    X₀, T₀ = random_sample(n_samples, X, T)
+    reshape_data(X₀, T₀)
 end
 
 function traindata(filtering_labels::Vector{Int}; dir = nothing)
-    filter_data(traindata(dir = dir)..., filtering_labels)
+    X, T = filter_data(MLDatasets.MNIST.traindata(dir = dir)..., filtering_labels)
+    reshape_data(X, T)
 end
 
 function traindata(n_samples::Int, filtering_labels::Vector{Int}; dir = nothing)
-    X, T = traindata(filtering_labels; dir = dir)
-    random_sample(n_samples, X, T)
+    X, T = filter_data(MLDatasets.MNIST.traindata(dir = dir)..., filtering_labels)
+    X₀, T₀ = random_sample(n_samples, X, T)
+    reshape_data(X₀, T₀)
 end
 
-# TODO Mimic MLDatasets API more closely.
 function testdata(; dir = nothing)
     X, T = MLDatasets.MNIST.testdata(dir = dir)
-    rows, cols, n = size(X)
-    (reshape(X, (rows * cols, n)), T)
+    reshape_data(X, T)
+end
+
+function testdata(n_samples::Int; dir = nothing)
+    X, T = MLDatasets.MNIST.testdata(dir = dir)
+    X₀, T₀ = random_sample(n_samples, X, T)
+    reshape_data(X₀, T₀)
 end
 
 function testdata(filtering_labels::Vector{Int}; dir = nothing)
-    filter_data(testdata(dir = dir)..., filtering_labels)
+    X, T = filter_data(MLDatasets.MNIST.testdata(dir = dir)..., filtering_labels)
+    reshape_data(X, T)
 end
 
 function testdata(n_samples::Int, filtering_labels::Vector{Int}; dir = nothing)
-    X, T = testdata(filtering_labels; dir = dir)
-    random_sample(n_samples, X, T)
+    X, T = filter_data(MLDatasets.MNIST.testdata(dir = dir)..., filtering_labels)
+    X₀, T₀ = random_sample(n_samples, X, T)
+    reshape_data(X₀, T₀)
 end
 
 function filter_data(samples::T1, labels::T2,
-                     filtering_labels::Vector{Int}) where {T1 <: AbstractMatrix, T2 <: AbstractVector}
+                     filtering_labels::Vector{Int}) where {T1 <: AbstractArray, T2 <: AbstractVector}
     indices = findall(x -> x ∈ filtering_labels, labels)
-    (samples[:,indices], labels[indices])
+    (samples[:,:,indices], labels[indices])
 end
 
-function random_sample(n_samples::Int, samples::T1, labels::T2) where {T1 <: AbstractMatrix, T2 <: AbstractVector}
+function reshape_data(samples::T1, targets::T2) where {T1 <: AbstractArray, T2 <: AbstractVector}
+    r, c, N = size(samples)
+    (reshape(samples, (r*c, N)), vector_to_1hot(targets))
+end
+
+function random_sample(n_samples::Int, samples::T1, labels::T2) where {T1 <: AbstractArray, T2 <: AbstractVector}
     @assert n_samples <= length(labels) "Blah."
     range = length(labels)
     indices = Random.randperm(range)[1:n_samples]
-    (samples[:,indices], labels[indices])
+    (samples[:,:,indices], labels[indices])
+end
+
+function vector_to_1hot(targets::T) where {T <: AbstractVector}
+    labels = Set(targets)
+    result = zeros(eltype(targets), length(labels), length(targets))
+    for (i, x) in enumerate(labels)
+        result[i,:] .= targets .== x
+    end
+    result
 end
